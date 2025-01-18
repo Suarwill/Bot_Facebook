32cc1dc01fee6469477ed934247e775db3753100
Your Name <you@example.com>
2025-01-18 16:29:58 -0300

    usando selenium

--- main.py
+++ main.py
@@ -20,11 +20,10 @@ from dotenv import load_dotenv
 libSetup('selenium')
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium.webdriver.common.alert import Alert
+
 
 def ventanaPublicar():
     ventanaPublicar = Toplevel(ventana)
